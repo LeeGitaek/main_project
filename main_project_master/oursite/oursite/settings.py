4a7a26d87948598fb6353fe8d08c7dd8b6c4d3a5
gitaek lee <coolfamily77@gmail.com>
2020-05-04 03:49:13 +0900

    Update settings.py

--- main_project_master/oursite/oursite/settings.py
+++ main_project_master/oursite/oursite/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
           'ENGINE': 'django.db.backends.mysql',
           'NAME': 'main_project', # DB명
           'USER': 'root', # 데이터베이스 계정
-          'PASSWORD': 'kor099060', # 계정 비밀번호
+          'PASSWORD': 'password', # 계정 비밀번호
           'HOST': 'localhost',# 데이테베이스 주소(IP)
           'PORT': '3306', # 데이터베이스 포트(보통은 3306)
       }
